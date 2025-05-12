976e8c8ad74df550f973938967a173228a81422f
Tuan Hung Pham <tomas5456@email.cz>
2025-05-12 12:15:20 +0200

    zprava

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.jsx'
 import './App.css'
-import Prevod_kurzu from './Components/Prevod_kurzu';
-
-
-
+import { Prevod } from './components/Prevod.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-   
-      <Prevod_kurzu />
+      <Prevod />
     </>
   )
 }
---
+++ src/Components/Prevod.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+export const Prevod = () => {
+    const [cislo, setCislo] = useState(0);
+    const [kurzy, setKurzy] = useState({});
+    const [vysledek, setVysledek] = useState(0);
+    const [vybrany, setVybrany] = useState(0);
+
+    const getKurzy = async () => {
+        try {
+            const response = await fetch("https://api.frankfurter.dev/v1/latest?base=EUR");
+            const data = await response.json();
+            setKurzy(data.rates);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getKurzy();
+    }, []);
+
+    const vypocitej = () => {
+        setVysledek(Number(cislo) * Number(vybrany));
+    };
+
+    return (
+        <div>
+            <h1>Převod EUR do jiné měny</h1>
+            <div>
+                <input
+                    type="number"
+                    value={cislo}
+                    onChange={(e) => setCislo(e.target.value)}
+                    style={{ padding: '5px' }}
+                />
+                <select onChange={(e) => setVybrany(e.target.value)}>
+                    {kurzy &&
+                        Object.entries(kurzy).map(([key, value]) => (
+                            <option value={value} key={key}>{key}</option>
+                        ))
+                    }
+                </select>
+                <button onClick={vypocitej}>Převeď</button>
+            </div>
+            <h3>{vysledek.toFixed(2)}</h3>
+        </div>
+    );
+};
